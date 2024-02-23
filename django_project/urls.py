2bd6a9bd9763c3ae900d726b302209efb2a79367
unknown <momenmedhatsalem@gmail.com>
2024-02-23 06:10:07 +0200

    ch17

--- django_project/urls.py
+++ django_project/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('anything-but-admin/', admin.site.urls),
     # user management
 
     path("accounts/", include("allauth.urls")),
